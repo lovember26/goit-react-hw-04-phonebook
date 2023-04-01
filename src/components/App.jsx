393a6c6be3e92e8175bfb125d801d02baa06eec3
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-04-01 22:28:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,25 @@ import { Wrapper, Title } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  componentDidMount() {
+    try {
+      const storageData = JSON.parse(localStorage.getItem('contacts'));
+
+      if (storageData) {
+        this.setState({ contacts: storageData });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   createContact = data => {
     this.state.contacts.some(el => el.name === data.name)
       ? alert(`${data.name} is already in contacts`)
